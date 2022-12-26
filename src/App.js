ef1d36df1b366e33da1a39a0053e659df6174db9
killuaDev <you@example.com>
2022-12-26 09:43:45 +0000

    Removed class based components in favour of function based ones, using setState

--- src/App.js
+++ src/App.js
@@ -1,14 +1,6 @@
 import './App.css';
 import React from 'react';
-
-/*
- * Proposed Structure:
- * TableWithOptions
- * 	Options
- * 	Table
- * 		Row
- * 			Content
- */
+import { useState } from 'react';
 
 function App() {
 	return (
@@ -23,108 +15,63 @@ function App() {
 }
 
 
-/* 
- * State will live here, because it needs to get to the rows from the options
- * Because of this, it will be a class rather than a function 
- */
-class TableWithOptions extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			standard: true, 
-			munster: false, 
-			connacht: false, 
-			ulster: false
-		}
+function TableWithOptions() {
+	const [standard, setStandard] = useState(true);
+	const [munster, setMunster] = useState(false);
+	const [connacht, setConnacht] = useState(false);
+	const [ulster, setUlster] = useState(false);
 
-		this.toggleStandard = this.toggleStandard.bind(this);
-		this.toggleMunster = this.toggleMunster.bind(this);
-		this.toggleConnacht = this.toggleConnacht.bind(this);
-		this.toggleUlster = this.toggleUlster.bind(this);
-		this.toggleDialect = this.toggleDialect.bind(this);
+	function toggleStandard() {
+		setStandard(!standard);
+	}
+	function toggleMunster() {
+		setMunster(!munster);
+	}
+	function toggleConnacht() {
+		setConnacht(!connacht);
+	}
+	function toggleUlster() {
+		setUlster(!ulster);
 	}
 
-	/*toggleDialect(dialect) {
-		this.setState(prevState => ({
-			dialect: !prevState.dialect // this is considering dialect the name of a property rather than as a variable that I passed
-		}));
-		
-	} */
-
-	/* This is the current workaround, it feels repetitive and unneccessary though */
-	toggleStandard() {
-		this.setState(prevState => ({standard: !prevState.standard}));
-	}
-	toggleMunster() {
-		this.setState(prevState => ({munster: !prevState.munster}));
-	}
-	toggleConnacht() {
-		this.setState(prevState => ({connacht: !prevState.connacht}));
-	}
-	toggleUlster() {
-		this.setState(prevState => ({ulster: !prevState.ulster}));
-	}
-
-	toggleDialect(dialect) {
-		switch (dialect) {
-			case "standard":
-				this.toggleStandard();
-				break;
-			case "munster":
-				this.toggleMunster();
-				break;
-			case "connacht":
-				this.toggleConnacht();
-				break;
-			case "ulster":
-				this.toggleUlster();
-				break;
-			default:
-				console.log("concern");
-		}
-	}
-
-
-
-
-	render () {
-		return (
-			<div className="TableWithOptions">
-				<Options enabled={this.state}
-					     toggleDialect={this.toggleDialect}
-				/>
-				<Table enabled={this.state}/>
-			</div>
-		); 
-	}
+	return (
+		<div className="TableWithOptions">
+			<Options standard={standard} toggleStandard={toggleStandard}
+					 munster={munster}   toggleMunster={toggleMunster}
+					 connacht={connacht} toggleConnacht={toggleConnacht}
+					 ulster={ulster}     toggleUlster={toggleUlster}
+			/>
+			<Table standard={standard}
+				   munster={munster}
+				   connacht={connacht}
+			       ulster={ulster}
+			/>
+		</div>
+	);
 }
 
 
-function Options(props) {
+function Options({standard, toggleStandard, munster, toggleMunster, connacht, toggleConnacht, ulster, toggleUlster}) {
 	return (
 		<div>
-			<button onClick={() => { 
-				props.toggleDialect("standard");
-				console.log("test");
-				console.log(props.enabled);
-				}}>
-				Standard:{props.enabled.standard ? "ON" : "OFF"}
+			<button onClick={toggleStandard}>
+				Standard:{standard ? "ON" : "OFF"}
 			</button>
-			<button onClick={() => props.toggleDialect("munster")}>
-				Munster:{props.enabled.munster ? "ON" : "OFF"}
-			</button>
-			<button onClick={() => props.toggleDialect("connacht")}>
-				Connacht:{props.enabled.connacht ? "ON" : "OFF"}
-			</button>
-			<button onClick={() => props.toggleDialect("ulster")}>
-				Ulster:{props.enabled.ulster ? "ON" : "OFF"}
+			<button onClick={toggleMunster}>
+				Munster:{munster ? "ON" : "OFF"}
+			</button>		
+			<button onClick={toggleConnacht}>
+				Connacht:{connacht ? "ON" : "OFF"}
+			</button>		
+			<button onClick={toggleUlster}>
+				Ulster:{ulster ? "ON" : "OFF"}
 			</button>
 		</div>
 	);
 }
 
 
-function Table(props) {
+function Table({standard, munster, connacht, ulster}) {
 	return (
 		<table>
 			<tr>
@@ -137,7 +84,7 @@ function Table(props) {
 				<td>Second Person Plural</td>
 				<td>Third Person Plural</td>
 			</tr>
-			<Row preposition="ag" enabled={props.enabled}/>
+			<Row preposition="ag" standard={standard} munster={munster} connacht={connacht} ulster={ulster}/>
 		</table>
 	);
 }
@@ -145,63 +92,60 @@ function Table(props) {
 
 /* Row() will be given a preposition and the dialects desired and it
  * will return a div with the relevant content */
-function Row(props) {  
+function Row({preposition, standard, munster, connacht, ulster}) {  
 	const indices = [0, 1, 2, 3, 4, 5, 6];
 	const cells = indices.map( (index) =>
-		<Cell enabled={props.enabled} index={index} preposition="ar"/>
+		<Cell standard={standard} index={index} preposition={preposition} munster={munster} connacht={connacht} ulster={ulster}/>
 	);
 	return (
 		<tr>
-			<td> {props.preposition} </td>
+			<td> {preposition} </td>
 			{cells}
 		</tr>
 	);
 }
 
-function Cell(props) {
-	const standard = {
+function Cell( {index, preposition, standard, munster, connacht, ulster} ) {
+	const standardList = {
 		ag: ["agam", "agat", "aige", "aici", "againn", "agaibh", "acu"],
 		ar: ["orm", "ort", "air", "uirthi", "orainn", "oraibh", "orthu"]
 	};
 
-	const connacht = {
+	const connachtList = {
 		ag: ["agam", "agat", "aige", "aici", "againn", "agaibh", "acu"],
 		ar: ["orm", "ort", "air", "uirthi", "orainn", "oraibh", "orthu"]
 	};
 
-	const ulster = {
+	const ulsterList = {
 		ag: ["agam", "agat", "aige", "aici", "againn", "agaibh", "acu"],
 		ar: ["orm", "ort", "air", "uirthi", "orainn", "oraibh", "orthu"]
 	};
 
-	const munster = {
+	const munsterList = {
 		ag: ["agum", "agut", "aige", "aici", "aguinn", "aguibh", "acu"],
 		ar: ["orm", "ort", "air", "uirthi", "orainn", "oraibh", "orthu"]
 	};
 
 	return (
 		<td> 
-			{props.enabled.standard &&
+			{standard &&
 				<div className="standard"> 
-					{standard[props.preposition][props.index]}
+					{standardList[preposition][index]}
 				</div>
 			}
-			
-			{props.enabled.munster &&
-				<div className="munster">
-					{munster[props.preposition][props.index]}
+			{munster &&
+				<div className="munster"> 
+					{munsterList[preposition][index]}
 				</div>
 			}
-
-			{props.enabled.connacht &&
-				<div className="connacht">
-					{connacht[props.preposition][props.index]}
+			{connacht &&
+				<div className="connacht"> 
+					{connachtList[preposition][index]}
 				</div>
 			}
-
-			{props.enabled.ulster &&
-				<div className="ulster">
-					{ulster[props.preposition][props.index]}
+			{ulster &&
+				<div className="ulster"> 
+					{ulsterList[preposition][index]}
 				</div>
 			}
 		</td>
